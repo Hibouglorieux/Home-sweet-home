a6c450496a6a1205a76305f42eb7fccd4eef4337
auto <auto@primb02.Tech-Info.heaj.edu>
2019-01-27 00:42:56 +0100

    test camera

---
+++ Home Sweet home/Assets/Scripts/test_camera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_camera : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 pos;
+	// Use this for initialization
+	void Start () {
+        pos = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = player.transform.position + pos;
+	}
+}
