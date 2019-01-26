6f8d0fcc03a6877db2ca0601c9d376b658204012
DavEat <oned@hotmail.be>
2019-01-26 18:09:47 +0100

    Add car event

---
+++ Home Sweet home/Assets/Scripts/Car.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Car : Interractable_item {
+
+    Animator _anim;
+
+    private void Awake()
+    {
+        needed_item = Drag_item.keys;
+    }
+
+    private void Start()
+    {
+        _anim = GetComponentInChildren<Animator>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+            StartEvent();
+    }
+
+    public override bool Launch_event(Drag_item item_held)
+    {
+        if (item_held == needed_item /*|| needed_item == Drag_item.none*/)
+        {
+            End_event();
+        }
+        return false;
+    }
+
+    public override void StartEvent()
+    {
+        _anim.SetTrigger("StartAlarm");
+    }
+
+    public override void End_event()
+    {
+        _anim.SetTrigger("CutAlarm");
+    }
+}
---
+++ Home Sweet home/Assets/Scripts/PlaySound.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour {
+
+    [SerializeField] AudioSource _source;
+
+    public void Play()
+    {
+        if (_source.isPlaying)
+            _source.time = 0;
+        _source.Play();
+    }
+}
