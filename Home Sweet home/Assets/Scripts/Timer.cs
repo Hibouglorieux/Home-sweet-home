485bb67519b0654f71b79de7e6a5617bc7f583a8
auto <auto@z4b16.Tech-Info.heaj.edu>
2019-01-26 05:55:23 +0100

    timer

---
+++ Home Sweet home/Assets/Scripts/Timer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public float max_timer;
+    [SerializeField] float actual_timer = 0;
+    private Image img;
+
+	void Start () {
+        transform.LookAt(Camera.main.transform);
+        transform.eulerAngles = (new Vector3(-transform.eulerAngles.x, 0, 0));
+        img = GetComponentInChildren<Image>();
+    }
+	
+	void Update () {
+        float percent = actual_timer / max_timer;
+        img.fillAmount = percent;
+        actual_timer += Time.deltaTime;
+        if (percent < 0.5)
+            img.color = new Color(percent * 2, 0.5f, img.color.b);
+        else
+            img.color = new Color(1, 1 - percent, img.color.b);
+    }
+}
