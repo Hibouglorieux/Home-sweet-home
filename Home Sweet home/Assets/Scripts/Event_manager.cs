b2cbf1a3978cf2cf439a745896f5c0255a70440d
auto <auto@primb02.Tech-Info.heaj.edu>
2019-01-27 15:31:49 +0100

    fixed event need to be tested

--- Home Sweet home/Assets/Scripts/Event_manager.cs
+++ Home Sweet home/Assets/Scripts/Event_manager.cs
@@ -5,25 +5,29 @@ using UnityEngine;
 public class Event_manager : MonoBehaviour {
 
     [SerializeField] int i;
-    int[] tab = new int[8]; // nombre d'evenements a faire pop
+    int[] tab = new int[9]; // nombre d'evenements a faire pop
     [SerializeField] int event_rate;
     [SerializeField] int random_event;
     [SerializeField] float Ending_timer = 120;
 
     public GameObject shelf;
+    public GameObject shelf2;
+    public GameObject shelf3;
+
     public GameObject fridge;
     public GameObject gaz;
     public GameObject car;
     public GameObject electricity;
     public GameObject cthulhu;
     public GameObject sink;
+    public GameObject sink2;
     public GameObject fruit;
 
 
     // Use this for initialization
     void Start() {
         i = 0;
-        for (int j = 0; j < 8; j++)
+        for (int j = 0; j < 9; j++)
             tab[j] = -1;
 
     }
@@ -43,22 +47,32 @@ public class Event_manager : MonoBehaviour {
 
     GameObject Chose_event(bool mod)
     {
-        int event_number = Random.Range(0, 8);
+        int event_number = Random.Range(0, 9);
         int count = 0;
-        for (int k = 0; k <= 7; k++)
+        for (int k = 0; k <= 8; k++)
         {
             if (mod == true)
                 break;
             if (event_number == tab[k])
                 return (Chose_event(true));
         }
-        while (count < 7  && tab[count] != -1)
+        while (count < 8 && tab[count] != -1)
             count++;
         tab[count] = event_number;
         switch (event_number)
         {
-            case 0:
-                return (shelf);
+            case 0: case 8:
+                {
+                    switch (Random.Range(0, 3))
+                    {
+                         case 0: default:
+                          return (shelf);
+                         case 1:
+                             return (shelf2);
+                         case 2:
+                             return (shelf3);
+                    }
+                }
             case 1:
                 return (fridge);
             case 2:
@@ -70,7 +84,16 @@ public class Event_manager : MonoBehaviour {
             case 5:
                 return (cthulhu);
             case 6:
-                return (sink);
+                {
+                    switch (Random.Range(0, 2))
+                    {
+                        case 0:
+                        default:
+                            return (sink);
+                        case 1:
+                            return (sink2);
+                    }
+                }
             case 7:
                 return (fruit);
             default:
