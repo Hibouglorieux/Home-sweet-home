edc96689a6b8ff121acafcee183391fdcd460e2b
DavEat <oned@hotmail.be>
2019-01-27 16:09:43 +0100

    Fix shelvf

--- Home Sweet home/Assets/Scripts/Shelf.cs
+++ Home Sweet home/Assets/Scripts/Shelf.cs
@@ -7,11 +7,13 @@ public class Shelf : Interractable_item {
     [SerializeField] Transform child;
     private bool ini = false;
     [SerializeField] AudioSource sound;
+    [SerializeField] Animator _anim;
     // Use this for initialization
     private void Awake()
     {
         needed_item = Drag_item.hammer;
-        duration_of_event = 20f;
+
+        _anim = GetComponent<Animator>();
     }
     public override bool Launch_event(Drag_item item_held)
     {
@@ -19,8 +21,6 @@ public class Shelf : Interractable_item {
         {
             Debug.Log("used"); //ok
                                // ajouter son
-            if (child.rotation.x < -0.038f)
-                child.eulerAngles += new Vector3(7, 0, 0);
             if (timer_script && (timer_script.actual_timer -= timer_script.max_timer / 15) < 0)
             {
                 Destroy(timer_script.gameObject);
@@ -34,7 +34,7 @@ public class Shelf : Interractable_item {
 
     public override void End_event()
     {
-        child.eulerAngles = new Vector3(0, -90, 0);
+        _anim.SetTrigger("fixed");
         ini = false;
     }
 
@@ -44,13 +44,9 @@ public class Shelf : Interractable_item {
         timer_script.max_timer = duration_of_event;
         timer_script.parentType = (int)Item.Interract_item.shelf;
 
+        _anim.SetTrigger("broken");
+
         sound.Play();
         ini = true;
     }
-
-    void Update () {
-        if (ini == true && child.rotation.x > -0.14f)
-             child.eulerAngles -= new Vector3(0.2f, 0, 0);
-		
-	}
 }
